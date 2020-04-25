bebb64d074b8ad3e9bbed4db72f65d8a4cf875e8
Robert Candler <robert.candler.32@gmail.com>
2020-04-24 22:45:19 -0600

    added employee

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Employee class
+//employee needs name, id, email, everyone is employee so only
+//the shared stuff goes here
+class employee {
+    constructor(id,name,email) {
+        this.id = id,
+        this.name = name,
+        this.email = email
+    }
+    getId() {return this.id}
+    getName() {return this.name}
+    getEmail() {return this.email}
+    //current job not specified so returns employee if not 
+    //specified later
+    getJob() {return "Employee"}
+}
+
+module.exports = employee
