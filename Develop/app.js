81f69d9d601a121e024d543422f19c65ec417697
Robert Candler <robert.candler.32@gmail.com>
2020-04-25 00:24:51 -0600

    made employee prompt and objects

--- Develop/app.js
+++ Develop/app.js
@@ -13,6 +13,111 @@ const render = require("./lib/htmlRenderer");
 ​
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+//create an array called team to put all employees in
+
+//create functions with prompts for all employee types
+function promptEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "id",
+            message: "What is the employee ID?"
+        }, {
+            type: "input",
+            name: "name",
+            message: "What is the employee name?"
+        }, {
+            type: "input",
+            name: "email",
+            message: "What is the employee email?"
+        }, {
+            type: "input",
+            name: "Github",
+            message: "What is the employee Github page?"
+        }
+    ]).then(function(ans) {
+        team.push(new Engineer(ans.id,ans.name,ans.email,ans.Github));
+        return team();
+    })
+}
+
+function promptIntern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "id",
+            message: "What is the employee ID?"
+        }, {
+            type: "input",
+            name: "name",
+            message: "What is the employee name?"
+        }, {
+            type: "input",
+            name: "email",
+            message: "What is the employee email?"
+        }, {
+            type: "input",
+            name: "School",
+            message: "What school does the intern go to?"
+        }
+    ]).then(function(ans) {
+        team.push(new Intern(ans.id,ans.name,ans.email,ans.School));
+        return team();
+    });
+}
+
+function promptManager() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "id",
+            message: "What is the employee ID?"
+        }, {
+            type: "input",
+            name: "name",
+            message: "What is the employee name?"
+        }, {
+            type: "input",
+            name: "email",
+            message: "What is the employee email?"
+        }, {
+            type: "input",
+            name: "OfficeNumber",
+            message: "What is the manager's office number?"
+        }
+    ]).then(function(ans) {
+        team.push(new Manager(ans.id,ans.name,ans.email,ans.OfficeNumber));
+        return team();
+    });
+}
+
+//create function to prompt user for a new employee, then use employee functions from above based on input
+function promptNew() {
+    inquirer.prompt([{
+        name: "name",
+        type: "list",
+        message: "Add new employee?",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager",
+            "No Thanks, I'm Done."
+        ]
+    }]).then(function(answer) {
+        if (answer.name === "Engineer") {
+            promptEngineer();
+        } else if (answer.name === "Intern") {
+            promptIntern();
+        } else if (answer.name === "Manager") {
+            promptManager();
+        } else if (answer.name === "No Thanks, I'm Done.") {
+            //ends successfully, now make html template
+        } else {
+            console.log("Something Went Wrong");
+        }
+    });
+}
 ​
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
